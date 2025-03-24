e6ce250a8134049d15e9ea4c6d671c0ce4139879
Anton Vasilyev <vasilyev@ispras.ru>
2025-03-24 15:45:49 +0300

    Separate color memory rule

--- klever/cli/descs/include/ldv/linux/device.h
+++ klever/cli/descs/include/ldv/linux/device.h
@@ -22,16 +22,6 @@
 
 struct device;
 
-extern struct drm_device;
-struct drm_driver;
-struct platform_device;
-struct drm_crtc;
-struct drm_crtc_funcs;
-struct drm_plane;
-struct drm_plane_funcs;
-struct drm_encoder;
-struct drm_encoder_funcs;
-
 extern void *ldv_dev_get_drvdata(const struct device *dev);
 extern int ldv_dev_set_drvdata(struct device *dev, void *data);
 extern void *ldv_devm_kmalloc(struct device *dev, size_t size, gfp_t gfp);
@@ -39,32 +29,6 @@ extern void *ldv_devm_kzalloc(struct device *dev, size_t size, gfp_t gfp);
 extern void *ldv_devm_kmalloc_array(size_t n, size_t size, gfp_t gfp);
 extern void *ldv_devm_kcalloc(size_t n, size_t size, gfp_t gfp);
 extern void ldv_devm_kfree(const void *p);
-
-extern void *ldv_drmm_kmalloc(struct drm_device *dev, size_t size, gfp_t gfp);
-extern void *ldv_drmm_kzalloc(struct drm_device *dev, size_t size, gfp_t gfp);
-
-extern int ldv_kref_put(struct kref *kref, void (*release)(struct kref *kref));
-extern void ldv_drm_dev_put(struct drm_device *dev);
-extern void ldv_kref_init(struct kref *kref);
-extern struct device *ldv_get_device(struct device *dev);
-extern void ldv_put_device(struct device *dev);
-extern struct drm_device *ldv_drm_dev_alloc(struct drm_driver *driver, struct device *parent);
-
-extern void ldv_devres_platform_set_drvdata(struct platform_device *pdev);
-extern const void *ldv_devres_of_device_get_match_data(const struct device *dev);
-extern void ldv_devres_dev_set_drvdata(struct device *dev, void *data);
-
-extern void *__ldv_devm_drm_dev_alloc(struct device *parent, struct drm_driver *driver, size_t size);
-
-extern int ldv_drmm_mode_config_init(struct drm_device *dev);
-extern void ldv_drm_mode_config_cleanup(struct drm_device *dev);
-extern void ldv_drm_crtc_cleanup(struct drm_crtc *crtc);
-extern int ldv_drm_crtc_init_with_planes(struct drm_device *dev, struct drm_crtc *crtc, const struct drm_crtc_funcs *funcs);
-extern void ldv_drm_plane_cleanup(struct drm_plane *plane);
-extern int ldv_drm_universal_plane_init(struct drm_device *dev, struct drm_plane *plane, const struct drm_plane_funcs *funcs);
-extern void ldv_drm_encoder_cleanup(struct drm_encoder *encoder);
-extern int ldv_drm_encoder_init(struct drm_device *dev, struct drm_encoder *encoder, const struct drm_encoder_funcs *funcs);
-extern int ldv_drm_simple_encoder_init(struct drm_device *dev, struct drm_encoder *encoder);
 
 extern int ldv_dev_err_probe(int err);
 #endif /* __LDV_LINUX_DEVICE_H */
