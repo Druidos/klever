eae327a5cb7fc91bc977afbfb1308174b08971bb
Katya Orlova <e.orlova@ispras.ru>
2025-04-17 17:07:10 +0300

    Add kvfree_call_rcu

--- presets/jobs/specifications/linux/kernel/rcu/rcu_with_memsafty.c
+++ presets/jobs/specifications/linux/kernel/rcu/rcu_with_memsafty.c
@@ -20,12 +20,17 @@
 
 int ldv_rcu_counter = 0;
 int ldv_rcu_callbacks_num = 0;
+int ldv_kfree_rcu_callbacks_num = 0;
 
-// локальная структура-список узлов для осовбождения
+// локальная структура-список узлов для осовбождения (call_rcu)
 struct rcu_head *old_rcu_heads;
-
-// указатель для удаляемого в текущий момент узла
+// указатель для удаляемого в текущий момент узла (call_rcu)
 struct rcu_head *curr_rcu_head;
+
+// локальная структура-список узлов для осовбождения (kfree_rcu)
+struct rcu_head *old_kfree_rcu_heads;
+// указатель для удаляемого в текущий момент узла (kfree_rcu)
+struct rcu_head *curr_kfree_rcu_heads;
 
 void ldv_rcu_read_lock( void )
 {
@@ -46,6 +51,17 @@ void ldv_rcu_read_unlock( void )
 		}
 		ldv_rcu_callbacks_num = 0;
 	}
+
+	if ((ldv_kfree_rcu_callbacks_num > 0) && (ldv_rcu_counter == 0))
+	{
+		// callback
+		while(old_kfree_rcu_heads != NULL) {
+			curr_kfree_rcu_heads = old_kfree_rcu_heads;
+			old_kfree_rcu_heads = curr_kfree_rcu_heads->next;
+			ldv_free(ptr);
+		}
+		ldv_kfree_rcu_callbacks_num = 0;
+	}
 }
 
 void ldv_call_rcu(struct rcu_head *head, rcu_callback_t func)
@@ -64,5 +80,26 @@ void ldv_call_rcu(struct rcu_head *head, rcu_callback_t func)
 	{
 		// callback
 		(*func)(head);
+	}
+}
+
+void ldv_kvfree_call_rcu(struct rcu_head *head, void *ptr)
+{
+	if (head) {
+        if (ldv_rcu_counter)
+        {
+            if(ldv_kfree_rcu_callbacks_num == 0)
+                old_kfree_rcu_heads = NULL;
+
+            ldv_kfree_rcu_callbacks_num += 1;
+            head->func = ptr;
+            head->next = old_kfree_rcu_heads;
+            old_kfree_rcu_heads = head;
+        }
+        else
+        {
+            // callback
+            ldv_free(ptr);
+        }
 	}
 }
