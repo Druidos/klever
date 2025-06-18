f85ee574ab6c0074da479df58b50653f6b0fd659
Katya Orlova <e.orlova@ispras.ru>
2025-06-18 13:51:10 +0300

    Fix model

--- presets/jobs/specifications/linux/memory safety/color_memory.c
+++ presets/jobs/specifications/linux/memory safety/color_memory.c
@@ -37,30 +37,36 @@
 extern struct devres *ldv_color_devm_kmalloc(size_t size, gfp_t gfp);
 extern void ldv_color_devm_kfree(struct devres *dr);
 
-extern struct devres *ldv_color_drm_kmalloc(size_t size, gfp_t gfp);
+extern void *ldv_color_drm_kmalloc(size_t size, gfp_t gfp);
 extern void ldv_color_drm_kfree(struct drmres *dr);
-extern void ldv_color_drm_dev_kfree(struct drm_dev *dev);
+extern void ldv_color_drm_dev_kfree(struct drm_device *dev);
+
+struct drm_links {
+	struct drm_plane *plane;
+	struct drm_crtc *crtc;
+	struct drm_encoder *encoder;
+} *links;
 
 struct drm_device *drm_dev;
 
 struct devres_node {
-	struct list_head		entry;
-	dr_release_t			release;
-  	const char				*name;
-  	size_t					size;
+	struct list_head entry;
+	dr_release_t release;
+	const char *name;
+	size_t size;
 };
 
 struct devres {
-	struct devres_node		node;
-    u8 data[];
+	struct devres_node node;
+	u8 data[];
 };
 
 
 struct drmres_node {
-	struct list_head	entry;
-	drmres_release_t	release;
-	const char		*name;
-	size_t			size;
+	struct list_head entry;
+	drmres_release_t release;
+	const char *name;
+	size_t size;
 };
 
 struct drmres {
@@ -88,12 +94,12 @@ void ldv_drm_mode_config_cleanup(struct drm_device *dev)
 	struct drm_plane *plane, *plt;
 
 	list_for_each_entry_safe(encoder, enct, &dev->mode_config.encoder_list,
-				 head) {
+							 head) {
 		encoder->funcs->destroy(encoder);
 	}
 
 	list_for_each_entry_safe(plane, plt, &dev->mode_config.plane_list,
-				 head) {
+							 head) {
 		plane->funcs->destroy(plane);
 	}
 
@@ -105,54 +111,58 @@ void ldv_drm_mode_config_cleanup(struct drm_device *dev)
 void ldv_drm_crtc_cleanup(struct drm_crtc *crtc)
 {
 	struct drm_device *dev = crtc->dev;
-	list_del(&crtc->head);
+	ldv_list_del(&crtc->head);
 	dev->mode_config.num_crtc--;
 }
 
-int ldv_drm_crtc_init_with_planes(struct drm_device *dev, struct drm_crtc *crtc,
-			      const struct drm_crtc_funcs *funcs)
+int ldv_drm_crtc_init_with_planes(struct drm_device *dev,
+								  struct drm_crtc *crtc,
+								  const struct drm_crtc_funcs *funcs)
 {
 	struct drm_mode_config *config = &dev->mode_config;
 	crtc->dev = dev;
 	crtc->funcs = funcs;
-	list_add_tail(&crtc->head, &config->crtc_list);
+	ldv_list_add_tail(&crtc->head, &config->crtc_list);
 	config->num_crtc++;
+	links->crtc = crtc;
 	return 0;
 }
 
 void ldv_drm_plane_cleanup(struct drm_plane *plane)
 {
 	struct drm_device *dev = plane->dev;
-	list_del(&plane->head);
+	ldv_list_del(&plane->head);
 	dev->mode_config.num_total_plane--;
 }
 
-int ldv_drm_universal_plane_init(struct drm_device *dev, struct drm_plane *plane,
-			     const struct drm_plane_funcs *funcs)
+int ldv_drm_universal_plane_init(struct drm_device *dev,
+								 struct drm_plane *plane,
+								 const struct drm_plane_funcs *funcs)
 {
-    struct drm_mode_config *config = &dev->mode_config;
-    plane->dev = dev;
-    plane->funcs = funcs;
-    list_add_tail(&plane->head, &config->plane_list);
+	struct drm_mode_config *config = &dev->mode_config;
+	plane->dev = dev;
+	plane->funcs = funcs;
+	ldv_list_add_tail(&plane->head, &config->plane_list);
 	config->num_total_plane++;
+	links->plane = plane;
 	return 0;
 }
 
 void ldv_drm_encoder_cleanup(struct drm_encoder *encoder)
 {
 	struct drm_device *dev = encoder->dev;
-	list_del(&encoder->head);
+	ldv_list_del(&encoder->head);
 	dev->mode_config.num_encoder--;
 }
 
-int ldv_drm_encoder_init(struct drm_device *dev,
-		     struct drm_encoder *encoder,
-		     const struct drm_encoder_funcs *funcs)
+int ldv_drm_encoder_init(struct drm_device *dev, struct drm_encoder *encoder,
+						 const struct drm_encoder_funcs *funcs)
 {
 	encoder->dev = dev;
 	encoder->funcs = funcs;
-	list_add_tail(&encoder->head, &dev->mode_config.encoder_list);
+	ldv_list_add_tail(&encoder->head, &dev->mode_config.encoder_list);
 	dev->mode_config.num_encoder++;
+	links->encoder = encoder;
 	return 0;
 }
 
@@ -161,9 +171,10 @@ static const struct drm_encoder_funcs ldv_drm_simple_encoder_funcs_cleanup = {
 };
 
 int ldv_drm_simple_encoder_init(struct drm_device *dev,
-			    struct drm_encoder *encoder)
+								struct drm_encoder *encoder)
 {
-	return ldv_drm_encoder_init(dev, encoder, &ldv_drm_simple_encoder_funcs_cleanup);
+	return ldv_drm_encoder_init(dev, encoder,
+								&ldv_drm_simple_encoder_funcs_cleanup);
 }
 
 
@@ -174,28 +185,25 @@ void *ldv_devres_dev_get_drvdata(const struct device *dev)
 
 void ldv_devres_dev_set_drvdata(struct device *dev, void *data)
 {
-    dev->driver_data = data;
+	dev->driver_data = data;
 }
 
 
-void *ldv_devm_kmalloc_array(size_t n, size_t size, gfp_t flags)
+void *ldv_devm_kmalloc_array(struct device *dev, size_t n, size_t size,
+							 gfp_t flags)
 {
 	void *res;
 
-	ldv_check_alloc_flags(flags);
-	res = ldv_malloc(n * size);
-	ldv_save_allocated_memory_to_list(res);
+	res = ldv_devm_kmalloc(dev, n * size, flags);
 
 	return res;
 }
 
-void *ldv_devm_kcalloc(size_t n, size_t size, gfp_t flags)
+void *ldv_devm_kcalloc(struct device *dev, size_t n, size_t size, gfp_t flags)
 {
 	void *res;
 
-	ldv_check_alloc_flags(flags);
-	res = ldv_calloc(n, size);
-	ldv_save_allocated_memory_to_list(res);
+	res = ldv_devm_kmalloc(dev, n * size, flags);
 
 	return res;
 }
@@ -208,8 +216,8 @@ struct devres * ldv_devm_alloc_dr(size_t size, gfp_t gfp, int flag)
 	if (!dr)
 		return NULL;
 
-    if(flag)
-        memset(dr, 0, offsetof(struct devres, data));
+	if(flag)
+		memset(dr, 0, offsetof(struct devres, data));
 
 	INIT_LIST_HEAD(&dr->node.entry);
 	return dr;
@@ -218,7 +226,7 @@ struct devres * ldv_devm_alloc_dr(size_t size, gfp_t gfp, int flag)
 void ldv_devres_add(struct device *dev, struct devres *dr)
 {
 	struct devres_node *node = &dr->node;
-  	list_add_tail(&node->entry, &dev->devres_head);
+	ldv_list_add_tail(&node->entry, &dev->devres_head);
 }
 
 void *ldv_devm_kmalloc(struct device *dev, size_t size, gfp_t gfp)
@@ -232,11 +240,11 @@ void *ldv_devm_kmalloc(struct device *dev, size_t size, gfp_t gfp)
 	dr->node.size = size;
 
 	ldv_devres_add(dev, dr);
-    struct devres_node *node = &dr->node;
-    if (list_empty(&dev->devres_head)) {
-        dev->devres_head.next = &node->entry;
-        node->entry.prev = &dev->devres_head;
-    }
+	struct devres_node *node = &dr->node;
+	if (ldv_list_empty(&dev->devres_head)) {
+		dev->devres_head.next = &node->entry;
+		node->entry.prev = &dev->devres_head;
+	}
 	return dr->data;
 }
 
@@ -251,11 +259,11 @@ void *ldv_devm_kzalloc(struct device *dev, size_t size, gfp_t gfp)
 	dr->node.size = size;
 
 	ldv_devres_add(dev, dr);
-    struct devres_node *node = &dr->node;
-    if (list_empty(&dev->devres_head)) {
-        dev->devres_head.next = &node->entry;
-        node->entry.prev = &dev->devres_head;
-    }
+	struct devres_node *node = &dr->node;
+	if (ldv_list_empty(&dev->devres_head)) {
+		dev->devres_head.next = &node->entry;
+		node->entry.prev = &dev->devres_head;
+	}
 	return dr->data;
 }
 
@@ -270,29 +278,48 @@ void ldv_release_nodes(struct device *dev, struct list_head *todo)
 
 int ldv_devres_release_all(struct device *dev)
 {
-
-	struct list_head todo = LIST_HEAD_INIT(todo);
-
-	if (list_empty(&dev->devres_head))
+	if (ldv_list_empty(&dev->devres_head))
 		return 0;
-	ldv_release_nodes(dev, &todo);
+	ldv_release_nodes(dev, &dev->devres_head);
 
 	return 0;
 }
 
+void ldv_devm_kfree(struct device *dev, const void *p)
+{
+	struct devres *dr_free = NULL, *dr;
+	struct devres_node *node;
+
+	if (!p)
+		return;
+
+	list_for_each_entry_reverse(node, &dev->devres_head, entry) {
+		dr = container_of(node, struct devres, node);
+
+		if (dr->data == p) {
+			dr_free = dr;
+			break;
+		}
+	}
+
+	if (dr_free) {
+		ldv_list_del_init(&dr_free->node.entry);
+		ldv_color_devm_kfree(dr_free);
+	}
+}
+
+
 static struct drmres * ldv_drmm_alloc_dr(drmres_release_t release,
-                                         size_t size, gfp_t gfp, int flag)
+										 size_t size, gfp_t gfp, int flag)
 {
 	struct drmres *dr;
-	if (!dr)
-		return NULL;
 
 	dr = ldv_color_drm_kmalloc(size + sizeof(*dr), gfp);
 	if (!dr)
 		return NULL;
 
-    if(flag)
-        memset(dr, 0, offsetof(struct drmres, data));
+	if(flag)
+		memset(dr, 0, offsetof(struct drmres, data));
 
 	INIT_LIST_HEAD(&dr->node.entry);
 	dr->node.release = release;
@@ -309,7 +336,7 @@ void *ldv_drmm_kmalloc(struct drm_device *dev, size_t size, gfp_t gfp)
 	if (!dr)
 		return NULL;
 
-	list_add(&dr->node.entry, &dev->managed.resources);
+	ldv_list_add(&dr->node.entry, &dev->managed.resources);
 
 	return dr->data;
 }
@@ -323,7 +350,7 @@ void *ldv_drmm_kzalloc(struct drm_device *dev, size_t size, gfp_t gfp)
 	if (!dr)
 		return NULL;
 
-	list_add(&dr->node.entry, &dev->managed.resources);
+	ldv_list_add(&dr->node.entry, &dev->managed.resources);
 
 	return dr->data;
 }
@@ -341,14 +368,9 @@ void ldv_drm_managed_release(struct drm_device *dev)
 		if (dr->node.release)
 			dr->node.release(dev, dr->node.size ? *(void **)&dr->data : NULL);
 
-		list_del(&dr->node.entry);
-      	ldv_color_drm_kfree(dr);
+		ldv_list_del(&dr->node.entry);
+		ldv_color_drm_kfree(dr);
 	}
-}
-
-static void ldv_del_dr(struct drm_device *dev, struct drmres *dr)
-{
-	list_del(&dr->node.entry);
 }
 
 void ldv_drmm_add_final_kfree(struct drm_device *dev, void *container)
@@ -357,27 +379,29 @@ void ldv_drmm_add_final_kfree(struct drm_device *dev, void *container)
 }
 
 int __ldv_drmm_add_action(struct drm_device *dev,
-		      drmres_release_t action,
-		      void *data)
+						  drmres_release_t action,
+						  void *data)
 {
 	struct drmres *dr;
 
 	dr = ldv_drmm_alloc_dr(action, 0, GFP_KERNEL, 0);
 	if (!dr)
-		return NULL;
+		return -1;
 
-	list_add(&dr->node.entry, &dev->managed.resources);
+	ldv_list_add(&dr->node.entry, &dev->managed.resources);
 
 	return 0;
 }
 
 int __ldv_drmm_add_action_or_reset(struct drm_device *dev,
-			       drmres_release_t action,
-			       void *data)
+								   drmres_release_t action,
+								   void *data)
 {
 	int ret;
 
 	ret = __ldv_drmm_add_action(dev, action, data);
+	if (ret)
+		action(dev, data);
 
 	return ret;
 }
@@ -392,11 +416,13 @@ void ldv_drmm_kfree(struct drm_device *dev, void *data)
 	list_for_each_entry(dr, &dev->managed.resources, node.entry) {
 		if (dr->data == data) {
 			dr_match = dr;
-			ldv_del_dr(dev, dr_match);
 			break;
 		}
 	}
-  	ldv_color_drm_kfree(dr);
+	if (dr_match) {
+		ldv_list_del(&dr_match->node.entry);
+		ldv_color_drm_kfree(dr_match);
+	}
 }
 
 
@@ -415,7 +441,7 @@ struct device *ldv_get_device(struct device *dev)
 
 void ldv_put_device(struct device *dev)
 {
-    struct kobject *kobj = &dev->kobj;
+	struct kobject *kobj = &dev->kobj;
 	struct kref *kref = &kobj->kref;
 	kref->refcount.refs.counter = kref->refcount.refs.counter - 1;
 }
@@ -427,8 +453,8 @@ static void ldv_drm_dev_init_release(struct drm_device *dev, void *res)
 }
 
 static int ldv_drm_dev_init(struct drm_device *dev,
-			const struct drm_driver *driver,
-			struct device *parent)
+							const struct drm_driver *driver,
+							struct device *parent)
 {
 	int ret = 0;
 
@@ -443,16 +469,18 @@ static int ldv_drm_dev_init(struct drm_device *dev,
 }
 
 struct drm_device *ldv_drm_dev_alloc(struct drm_driver *driver,
-				 struct device *parent)
+									 struct device *parent)
 {
 	struct drm_device *dev;
 
 	dev = ldv_color_drm_kmalloc(sizeof(*dev), GFP_KERNEL);
 	if (!dev)
 		return NULL;
-    drm_dev = dev;
+	drm_dev = dev;
 
-  	ldv_devres_dev_set_drvdata(parent, dev);
+	links = ldv_color_drm_kmalloc(sizeof(*links), GFP_KERNEL);
+
+	ldv_devres_dev_set_drvdata(parent, dev);
 
 	ldv_drm_dev_init(dev, driver, parent);
 
@@ -465,11 +493,11 @@ static void ldv_drm_dev_release(struct kref *ref)
 {
 	struct drm_device *dev = container_of(ref, struct drm_device, ref);
 
-    if(drm_dev) {
-         ldv_drm_managed_release(dev);
-         ldv_color_drm_dev_kfree(dev->managed.final_kfree);
-    }
-
+	if(drm_dev) {
+		ldv_drm_managed_release(dev);
+		ldv_color_drm_dev_kfree(dev->managed.final_kfree);
+		ldv_color_drm_dev_kfree(links);
+	}
 }
 
 int ldv_kref_put(struct kref *kref, void (*release)(struct kref *kref))
@@ -488,25 +516,29 @@ void ldv_drm_dev_put(struct drm_device *dev)
 		ldv_kref_put(&dev->ref, ldv_drm_dev_release);
 }
 
-void *__ldv_devm_drm_dev_alloc(struct device *parent, struct drm_driver *driver, size_t size)
+void *__ldv_devm_drm_dev_alloc(struct device *parent, struct drm_driver *driver, size_t size, size_t offset)
 {
-    struct drm_device *dev;
+	void *container;
+	struct drm_device *dev;
 
-	dev = ldv_color_drm_kmalloc(size, GFP_KERNEL);
-	if (!dev)
+	container = ldv_color_drm_kmalloc(size, GFP_KERNEL);
+	if (!container)
 		return NULL;
-    drm_dev = dev;
+	dev = container + offset;
+	drm_dev = dev;
 
-  	ldv_devres_dev_set_drvdata(parent, dev);
+	links = ldv_color_drm_kmalloc(sizeof(*links), GFP_KERNEL);
+
+	ldv_devres_dev_set_drvdata(parent, dev);
 
 	ldv_drm_dev_init(dev, driver, parent);
 
-	ldv_drmm_add_final_kfree(dev, dev);
+	ldv_drmm_add_final_kfree(dev, container);
 
-	return dev;
+	return container;
 }
 
 const void *ldv_devres_of_device_get_match_data(const struct device *dev) {
-    int ret = 1;
-    return ret;
+	int ret = 1;
+	return ret;
 }
